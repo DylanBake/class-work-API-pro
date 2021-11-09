a08953148e989dc30a16067cc76c7917b205a2ca
Dylan Bake <92402722+DylanBake@users.noreply.github.com>
2021-11-09 22:12:54 +0000

    last thing

--- main.py
+++ main.py
@@ -6,4 +6,18 @@ query = ""
 url = url_host + endpoint# + query
 
 response = requests.get(url)
-print(response.json())
+print(response.json()["message"]
+
+#get a list of all breeds from the API call
+all_breeds = breeds.keys()
+
+# loop and print out each Key 
+for breed in all_breeds:
+  print(breed)
+
+
+# display all retreiver subbreeds
+retriever_subbreeds = breeds["retriever"]
+input("\nPress enter to get all subbreeds")
+for sub in retriever_subbreeds:
+  print(sub)
